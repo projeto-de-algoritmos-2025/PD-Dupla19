046de2c2a9b911a4ac6853e1edf98df00ede65fc
Bruno Cruz <Bruno.g.rosa@hotmail.com.br>
2025-12-01 22:43:20 -0300

    ajustes gerais de erros e otimização

--- resolucao474.cpp
+++ resolucao474.cpp
@@ -9,43 +9,34 @@ class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
 
-        int L = strs.size();
-        vector<vector<vector<int>>> dp(
-            L + 1,
-            vector<vector<int>>(m + 1, vector<int>(n + 1, 0))
-        );
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        // pr-conta zeros e uns
-        vector<pair<int,int>> count(L);
-        for (int i = 0; i < L; i++) {
+        // pre-conta zeros e uns
+        vector<pair<int,int>> count;
+        for (string &s : strs) {
             int zeros = 0, ones = 0;
-            for (char c : strs[i]) {
+            for (char c : s) {
                 if (c == '0') zeros++;
                 else ones++;
             }
-            count[i] = {zeros, ones};
+            count.push_back({zeros, ones});
         }
 
-        for (int i = 1; i <= L; i++) {
-            int z = count[i - 1].first;
-            int o = count[i - 1].second;
+        // atualizar de trás pra frente
+        for (auto &p : count) {
+            int z = p.first;
+            int o = p.second;
 
-            for (int mm = 0; mm <= m; mm++) {
-                for (int nn = 0; nn <= n; nn++) {
-                    // não pegar
-                    dp[i][mm][nn] = dp[i - 1][mm][nn];
-
-                    // pegar 
-                    if (mm >= z && nn >= o) {
-                        dp[i][mm][nn] = max(
-                            dp[i][mm][nn],
-                            1 + dp[i - 1][mm - z][nn - o]
-                        );
-                    }
+            for (int mm = m; mm >= z; mm--) {
+                for (int nn = n; nn >= o; nn--) {
+                    dp[mm][nn] = max(
+                        dp[mm][nn],
+                        1 + dp[mm - z][nn - o]
+                    );
                 }
             }
         }
 
-        return dp[L][m][n];
+        return dp[m][n];
     }
 };
