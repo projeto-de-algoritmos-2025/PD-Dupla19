7844b13950dddd024dca7bdfd4421ab466550e76
Bruno Cruz <Bruno.g.rosa@hotmail.com.br>
2025-12-01 22:40:10 -0300

    melhoria usando matriz

--- resolucao474.cpp
+++ resolucao474.cpp
@@ -8,26 +8,44 @@ using namespace std;
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        return solve(strs, 0, m, n);
-    }
 
-    int solve(vector<string>& strs, int index, int m, int n) {
-        if (index == strs.size()) return 0;
+        int L = strs.size();
+        vector<vector<vector<int>>> dp(
+            L + 1,
+            vector<vector<int>>(m + 1, vector<int>(n + 1, 0))
+        );
 
-        int zeros = 0, ones = 0;
-        for (char c : strs[index]) {
-            if (c == '0') zeros++;
-            else ones++;
+        // pr-conta zeros e uns
+        vector<pair<int,int>> count(L);
+        for (int i = 0; i < L; i++) {
+            int zeros = 0, ones = 0;
+            for (char c : strs[i]) {
+                if (c == '0') zeros++;
+                else ones++;
+            }
+            count[i] = {zeros, ones};
         }
 
-        // não pegar
-        int best = solve(strs, index + 1, m, n);
+        for (int i = 1; i <= L; i++) {
+            int z = count[i - 1].first;
+            int o = count[i - 1].second;
 
-        // pegar 
-        if (zeros <= m && ones <= n) {
-            best = max(best, 1 + solve(strs, index + 1, m - zeros, n - ones));
+            for (int mm = 0; mm <= m; mm++) {
+                for (int nn = 0; nn <= n; nn++) {
+                    // não pegar
+                    dp[i][mm][nn] = dp[i - 1][mm][nn];
+
+                    // pegar 
+                    if (mm >= z && nn >= o) {
+                        dp[i][mm][nn] = max(
+                            dp[i][mm][nn],
+                            1 + dp[i - 1][mm - z][nn - o]
+                        );
+                    }
+                }
+            }
         }
 
-        return best;
+        return dp[L][m][n];
     }
 };
